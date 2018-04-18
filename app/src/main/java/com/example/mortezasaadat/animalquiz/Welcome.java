2ba2fb061933eaaac159894c9bed6112e6751ed2
Deepak Vishwakarma <vishwakarmadeepak310@gmail.com>
2018-04-18 15:58:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mortezasaadat/animalquiz/Welcome.java
@@ -0,0 +1,14 @@
+package com.example.mortezasaadat.animalquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ippWelcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
+''
